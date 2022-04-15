e78d3d300292ed89d49d197f67272d89f22ed276
Luis Felipe Nunes <tu.massa@hotmail.com>
2022-04-15 10:49:09 -0300

    Project's initial commit

---
+++ src/main/java/com/example/pocJavaMongoDB/PocJavaMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocJavaMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pocJavaMongoDB/PocJavaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocJavaMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
